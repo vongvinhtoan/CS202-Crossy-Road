9ceddb9129318fed4256b0ed482191320a905983
Nam PHAM <127522074+hhannaap@users.noreply.github.com>
2023-11-24 21:56:12 +0700

    set ...

--- src/Activities/CharacterActivity.cpp
+++ src/Activities/CharacterActivity.cpp
@@ -1,20 +1,20 @@
 #include <Activities/CharacterActivity.hpp>
 
-CharacterActivity::CharacterActivity(ActivityStack& stack, int requestCode, Extra& intent)
-    : Activity(stack, requestCode, intent) {
+CharacterActivity::CharacterActivity(ActivityStack &stack, int requestCode, Extra &intent)
+    : Activity(stack, requestCode, intent)
+{
     setupCharacterVector();
 
     std::cout << "CharacterActivity::CharacterActivity()" << std::endl;
-    ViewNode* background_layer = getLayer(0);
-    ViewNode* ui_layer = getLayer(1);
+    ViewNode *background_layer = getLayer(0);
+    ViewNode *ui_layer = getLayer(1);
 
     auto window = getContext()->getWindow();
     sf::Vector2f window_size(window->getSize());
 
     // background_layer
     auto background = std::make_unique<RectangleView>(window_size);
-    background->get().setTexture(&getContext()->getTextures()->get(TextureID::Background)
-    );
+    background->get().setTexture(&getContext()->getTextures()->get(TextureID::Background));
     background_layer->attachChild(std::move(background));
 
     // ui_layer
@@ -24,39 +24,55 @@ CharacterActivity::CharacterActivity(ActivityStack& stack, int requestCode, Extr
     auto leftArrow = std::make_unique<RectangleView>(sf::Vector2f(115, 165));
     leftArrow->get().setTexture(&getContext()->getTextures()->get(TextureID::LeftArrow));
     leftArrow->get().setPosition(sf::Vector2f(64.f, 383.f));
-
+    leftArrow->setOnClick([&](ViewNode &clickedNode)
+                          {
+        currentCharacterIndex--;
+        showCharacter(currentCharacterIndex, ui_layer);
+        std::cout << "Clicked leftArrow" << std::endl;
+        std::cout << "currentCharacterIndex: " << currentCharacterIndex << std::endl; });
     // std::unique_ptr<ViewNode> leftArrowPtr = static_cast<std::unique_ptr<ViewNode>>(leftArrow.get());
     // leftArrowPtr->setOnClick([&](ViewNode& clickedNode) {
     //     currentCharacterIndex--;
-    //     showCharacter(currentCharacterIndex, ui_layer);
+    // showCharacter(currentCharacterIndex, ui_layer);
     // });
 
     ui_layer->attachChild(std::move(leftArrow));
 
     // rightArrow
     auto rightArrow = std::make_unique<RectangleView>(sf::Vector2f(115, 165));
-    rightArrow->get().setTexture(&getContext()->getTextures()->get(TextureID::RightArrow)
-    );
+    rightArrow->get().setTexture(&getContext()->getTextures()->get(TextureID::RightArrow));
     rightArrow->get().setPosition(sf::Vector2f(1413.f, 383.f));
+    rightArrow->setOnClick([&](ViewNode &clickedNode)
+                           {
+        currentCharacterIndex++;
+        std::cout << "Clicked rightArrow" << std::endl;
+        std::cout << "currentCharacterIndex: " << currentCharacterIndex << std::endl; 
+        showCharacter(currentCharacterIndex, ui_layer); });
     ui_layer->attachChild(std::move(rightArrow));
 }
 
-void CharacterActivity::setupCharacterVector() {
+void CharacterActivity::setupCharacterVector()
+{
     addCharacterTexture(TextureID::CharacterPenguin);
     addCharacterTexture(TextureID::CharacterChicken);
     addCharacterTexture(TextureID::CharacterMonkey);
 }
 
-void CharacterActivity::showCharacter(int index, ViewNode* ui_layer) {
+void CharacterActivity::showCharacter(int index, ViewNode *ui_layer)
+{
+    std::cout << "CharacterActivity::showCharacter()" << std::endl;
     auto centralCharacter = std::make_unique<RectangleView>(sf::Vector2f(512, 512));
     centralCharacter->get().setTexture(mCharacter[index].get());
     centralCharacter->get().setPosition(sf::Vector2f(544.f, 238.f));
     ui_layer->attachChild(std::move(centralCharacter));
 
     auto leftCharacter = std::make_unique<RectangleView>(sf::Vector2f(260, 288));
-    if (index > 0) {
+    if (index > 0)
+    {
         leftCharacter->get().setTexture(mCharacter[index - 1].get());
-    } else {
+    }
+    else
+    {
         leftCharacter->get().setFillColor(sf::Color::Transparent);
     }
 
@@ -64,9 +80,12 @@ void CharacterActivity::showCharacter(int index, ViewNode* ui_layer) {
     ui_layer->attachChild(std::move(leftCharacter));
 
     auto rightCharacter = std::make_unique<RectangleView>(sf::Vector2f(260, 288));
-    if (index < mCharacter.size() - 1) {
+    if (index < mCharacter.size() - 1)
+    {
         rightCharacter->get().setTexture(mCharacter[index + 1].get());
-    } else {
+    }
+    else
+    {
         rightCharacter->get().setFillColor(sf::Color::Transparent);
     }
 
@@ -74,20 +93,23 @@ void CharacterActivity::showCharacter(int index, ViewNode* ui_layer) {
     ui_layer->attachChild(std::move(rightCharacter));
 }
 
-void CharacterActivity::addCharacterTexture(TextureID::ID characterID) {
+void CharacterActivity::addCharacterTexture(TextureID::ID characterID)
+{
     auto texture = getContext()->getTextures()->get(characterID);
     mCharacter.push_back(std::make_unique<sf::Texture>(texture));
 }
 
 CharacterActivity::~CharacterActivity() {}
 
-bool CharacterActivity::handleEvent(sf::Event& event) {
+bool CharacterActivity::handleEvent(sf::Event &event)
+{
     return Activity::handleEvent(event);
 }
 
 bool CharacterActivity::handleRealtimeInput() { return Activity::handleRealtimeInput(); }
 
-bool CharacterActivity::update(sf::Time dt) {
+bool CharacterActivity::update(sf::Time dt)
+{
     Activity::update(dt);
     return 0;
 }
